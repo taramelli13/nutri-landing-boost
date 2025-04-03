9575d5680bf7e457d106816597407c6b5669ecdc
Ygor Taramelli <nutriygortaramelli@gmail.com>
2025-04-02 23:19:34 -0300

    feat: deploy para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
+const repoName = 'nutri-landing-boost';
+
 export default defineConfig(({ mode }) => ({
+  base: `/${repoName}/`,
   server: {
     host: "::",
     port: 8080,
