d218e5b6f81981572c969dc4f117ceafc240c41f
taramelli13 <nutriygortaramelli@gmail.com>
2025-04-03 15:10:10 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,19 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+export const initGoogleAnalytics = () => {
+  const GA_MEASUREMENT_ID = "G-T93BEVHBL4";
 
-createRoot(document.getElementById("root")!).render(<App />);
+  const script = document.createElement("script");
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(...args: any[]) {
+    window.dataLayer.push(args);
+  }
+
+  gtag("js", new Date());
+  gtag("config", GA_MEASUREMENT_ID);
+  
+  // @ts-ignore
+  window.gtag = gtag;
+};
